abf9fe076510a117a0030c436d25df0865970449
orlando <ninaorellanaorlando@gmail.com>
2025-07-25 22:09:23 +0100

    add test to check if all films are film intances

--- tests/films/ListFilms.test.ts
+++ tests/films/ListFilms.test.ts
@@ -1,8 +1,13 @@
 import { describe, it, expect } from 'vitest';
+import { Film } from '../../src/films/domain/Film.ts';
 import { listFilms } from '../../src/films/application/ListFilms.ts';
 
 describe('listFilms', () => {
   it('returns all films', () => {
-    expect(true).toBeTruthy;
+    const films = listFilms();
+
+    for (const film of films) {
+      expect(film).toBeInstanceOf(Film);
+    }
   });
 });
