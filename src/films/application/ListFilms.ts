0f978d8da1e69e7f8346a3c74030e5f862c3053c
orlando <ninaorellanaorlando@gmail.com>
2025-07-25 22:12:03 +0100

    remove unnecessary typing

--- src/films/application/ListFilms.ts
+++ src/films/application/ListFilms.ts
@@ -1,10 +1,10 @@
 import { Film } from '../domain/Film.ts';
 
 export function listFilms(): Film[] {
-  let films: Film[] = [];
+  let films = [];
   for (let i = 0; i < 10; ++i) {
-    let index: string = i.toString();
-    let film: Film = new Film(index, new Date(index), index, ['Array', index]);
+    let index = i.toString();
+    let film = new Film(index, new Date(index), index, ['Array', index]);
     films.push(film);
   }
   return films;
