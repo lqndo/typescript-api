bcf2b03346de88f9b9b9bdb4af69467967c9f2e6
orlando <ninaorellanaorlando@gmail.com>
2025-07-25 21:57:10 +0100

    add vitest rootconfig

---
+++ tests/films/ListFilms.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+import { listFilms } from '../../src/films/application/ListFilms.ts';
+
+describe('listFilms', () => {
+  it('returns all films', () => {
+    expect(true).toBeTruthy;
+  });
+});
